6805cbf2e5b32530895866c282c2025b1d71423e
Patti O'Krongly <pattio@Pattis-MacBook.local>
2019-07-11 10:08:16 -0500

    added customer list

---
+++ LemonadeStand/Day.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LemonadeStand
+{
+    public class Day
+    {
+        public List<Customer> customerList;
+        public string weather;
+        
+        public Day(string weather)
+        {
+
+            //this.weather = forecastWeather;
+            customerList = new List<Customer>();
+            {
+                Customer.Add("Zombie")
+            }
+        }
+
+    }
+}
+
