35493336fcc3f1f9cb4ef875cbab8dd32d4184b1
Patti O'Krongly <pattio@Pattis-MacBook.local>
2019-07-12 13:11:15 -0500

    added methods to player class, working on customer purchase of lemonade

--- LemonadeStand/Player.cs
+++ LemonadeStand/Player.cs
@@ -1,17 +1,66 @@
 ﻿using System;
+
 namespace LemonadeStand
+
 {
     public class Player
     {
-        Player player = new Player();
-        Console.WriteLine(player.Lemon);
-        player.Lemon -= 50;
-        Console.WriteLine(player.Lemon);
+
+        double money;
+        Inventory inventory = new Inventory();
 
 
-        public Player()
+
+
+        public void BuyLemons()
         {
+            Console.WriteLine("How many lemons do you want? They each cost .10.");
+            string numberOfLemons = Console.ReadLine();
+            int Lemons = Int32.Parse(numberOfLemons);
+
+            double LemonPrice = .10;
+            double Cost = LemonPrice * Lemons;
+
+            if (money >= Cost)
+            {
+                money -= Cost;
+                inventory.Lemon += Lemons;
+
+            }
         }
+        public void BuySugar()
+        {
+            Console.WriteLine("How many cups of sugar do you want? Each cup cost .25.");
+            string cupsOfSugar = Console.ReadLine();
+            int Sugar = Int32.Parse(cupsOfSugar);
+
+            double SugarPrice = .25;
+            double CostOfSugar = SugarPrice * Sugar;
+
+            if (money >= CostOfSugar)
+            {
+                money -= CostOfSugar;
+                inventory.Sugar += Sugar; 
+
+            }
+
+        }
+        public void BuyIce()
+        {
+            Console.WriteLine("How many ice cubes would you like to make your Lemonade ice cold? They cost .03 each.");
+            string iceCubes = Console.ReadLine();
+            int Ice = Int32.Parse(iceCubes);
+
+            double IcePrice = .03;
+            double CostofIce = IcePrice * Ice;
+
+            if (money >= CostofIce)
+            {
+
+                money -= CostofIce;
+                inventory.Ice += Ice;
+        
+            }
     }
     
 }
